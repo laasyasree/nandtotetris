b5813f7f9a4590f05c45bedb780cc5b05715cadb
Laasya <laasyasreesapa@gmail.com>
2019-10-12 15:06:39 +0530

    Update VMTranslator.py

--- 07/VMTranslator.py
+++ 07/VMTranslator.py
@@ -1,9 +1,8 @@
-class Parser(self,inputfile):
+class Parser:
 	def __init__(self,inputfile):
 		self.inputfile=open(inputfile,'r')
-		self.command=[""]
-		self.cursorAtEnd = False
-		self.cType={
+		self.line=[""]
+		self.commands={
 			"add" : "C_ARITHMETIC",
             "sub" : "C_ARITHMETIC",
             "neg" : "C_ARITHMETIC",
@@ -13,18 +12,107 @@ class Parser(self,inputfile):
             "and" : "C_ARITHMETIC",
             "or" : "C_ARITHMETIC",
             "not" : "C_ARITHMETIC",
-            "push" : "push",
-            "pop" : "pop",
-            "EOF" : "C_EOF",
+            "pop": "C_POP",
+            "push": "C_PUSH"
             }
+       self.end= False
+            
 	def hasMoreCommands(self):
+		if(self.end):
+			return False
+		else:
+			return True
 		
 		
 	def advance(self):
-		line=self.inputfile.readline()
-		if(line==""):
-			self.cursorAtEnd = True
+		a=self.inputfile.readline()
+		if(a==""):
+			self.end = True
 		else:
+			line=a.strip()
+			components=line.split()
+			
+			
+			
+	def commandType(self):
+		return self.commands.get(components[0],"invalid")
+		
+	def arg1(self):
+		return self.components[1]
+		
+	def arg2(self):
+		return self.components[2]
+		
+class CodeWriter:
+	def __init__(self,outputfile):
+		self.outputfile.open(outputfile,"w")
+		
+	def Close(self):
+		self.outputfile.close()
+		
+	def writeArithmetic(self,components):
+		smtg=""
+		if(components=="add"):
+			smtg+="@SP\n"
+			smtg+="A=M-1\n"
+			smtg+="D=M\n"
+			smtg+="@SP\n"
+			smtg+="A=M-1\n"
+			smtg+="M=D+M\n"
+			smtg+="@SP\n"
+			smtg+="M=M+1\n"
+			
+		elif(components=="sub"):
+			smtg+="@SP\n"
+			smtg+="A=M-1\n"
+			smtg+="D=M\n"
+			smtg+="@SP\n"
+			smtg+="A=M-1\n"
+			smtg+="M=D-M\n"
+			smtg+="@SP\n"
+			smtg+="M=M+1\n"
+			
+		elif(components=="neg"):
+			smtg+="@SP\n"
+			smtg+="A=M-1\n"
+			smtg+="M=-M\n"
+			
+		elif(components=="eq"):
+			smtg+="@SP\n"
+			smtg+="A=M-1\n"
+			smtg+="D=M\n"
+			smtg+="@SP\n"
+			smtg+="A=M-1\n"
+			smtg+="D=D-M\n"
+			smtg+="@equal\n"
+			smtg+="D;JEQ\n"
+			
+		elif(components=="gt"):
+			smtg+="@SP\n"
+			smtg+="A=M-1\n"
+			smtg+="D=M\n"
+			smtg+="@SP\n"
+			smtg+="A=M-1\n"
+			smtg+="D=D-M\n"
+			smtg+="@greaterthan\n"
+			smtg+="D;JGT\n"
+			
+		elif(components=="lt"):
+			smtg+="@SP\n"
+			smtg+="A=M-1\n"
+			smtg+="D=M\n"
+			smtg+="@SP\n"
+			smtg+="A=M-1\n"
+			smtg+="D=D-M\n"
+			smtg+="@dc\n"
+			smtg+="D;JLTn"
+			
+			
+		
+		
+
+	
+		
 			
 			
 			
