149a23b5f29f6da41c2ac38468f8bafbe3607c0f
Laasya <laasyasreesapa@gmail.com>
2019-10-15 21:03:42 +0530

    Update VMTranslator.py

--- 07/VMTranslator.py
+++ 07/VMTranslator.py
@@ -366,28 +366,3 @@ if __name__ == "__main__":
 	import sys
 	sys.exit(main(sys.argv))
 				
-		
-				
-			
-				
-				
-			
-		
-			
-			
-		
-		
-
-	
-		
-			
-			
-			
-			
-			
-		
-			
-			
-		
-	
-
